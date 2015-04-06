68b053b519c8d2e5c3174b9864782fe82d47e6a3
Daniel Sont <dan.sont@gmail.com>
2015-04-07 03:19:29 +0700

    Team w/ Photos

---
+++ client/src/components/team/index.js
@@ -0,0 +1,31 @@
+var thumbnailGrid = require('./thumbnail-grid.ejs');
+var style = require('./thumbnail-grid.css');
+var thumbnail = require('./thumbnail.ejs');
+
+var thumbnails = [{
+  caption: 'Dan',
+  url: '/team-dan.jpg'
+}, {
+  caption: 'Jake',
+  url: '/team-jake.png'
+}, {
+  caption: 'Hai Bac',
+  url: '/team-haibac.png'
+}, {
+  caption: 'Tuan',
+  url: '/team-tuan.png'
+}, {
+  caption: 'Dung',
+  url: '/team-minhha.png'
+}];
+
+var inner = '';
+for (var i = 0; i < thumbnails.length; i++) {
+  inner += thumbnail(thumbnails[i]);
+}
+
+module.exports = function () {
+  return thumbnailGrid({
+    thumbnails: inner
+  });
+};
---
+++ client/src/content/team.md
@@ -0,0 +1,11 @@
+
+#### Team & Photos
+
+We all love tech, business, writing, hardware, software, linux, IoT and the likes. It is difficult to distinguish our roles, at times. So for now, countries will do.
+
+- Jacob Gadikian - United States
+- Daniel Sont - Canada
+- Hai Bac - Vietnam
+- Tuan - Vietnam
+- Dung (Manufacturer) - Vietnam
+- Dung (Investor) - Vietnam
